39a7844fe8ba4739f90dd73ef6e5f316d4fdab98
Victor <santosfranciscosouza310@gmail.com>
2023-02-03 21:21:13 -0300

    Create class ChessPosition

--- Chess-console/Program.cs
+++ Chess-console/Program.cs
@@ -6,22 +6,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        try
-        {
-            Board chessbd = new Board(8, 8);
-            chessbd.PutPiece(new Tower(chessbd, Color.black), new Position(0, 0));
-            chessbd.PutPiece(new King(chessbd, Color.black), new Position(0, 3));
-            chessbd.PutPiece(new Tower(chessbd, Color.black), new Position(2, 4));
-
-            Screen.PrintBoard(chessbd);
-        }
-        catch(BoardException e)
-        {
-            Console.WriteLine("Board error: "+ e.Message);
-        }
-        catch(Exception ex)
-        {
-            Console.WriteLine("Unexpected error: "+ex.Message);
-        }
+        ChessPosition position = new ChessPosition('c', 7);
+        Console.WriteLine(position);
+        Console.WriteLine(position.ToPosition());
     }
 }
---
+++ Chess-console/chess/ChessPosition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using boardgame;
+namespace chess
+{
+    internal class ChessPosition
+    {
+        public int row { get; set; }
+        public char column { get; set; }
+        public ChessPosition(char column, int row)
+        {
+            this.row = row;
+            this.column = column;
+        }
+        public Position ToPosition()
+        {
+            return new Position(8 - row, column - 'a');
+        }
+        public override string ToString()
+        {
+            return ""+column+row;
+        }
+    }
+}
